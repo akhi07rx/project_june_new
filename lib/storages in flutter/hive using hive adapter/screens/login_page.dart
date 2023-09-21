5005cda61de59d589ef08f034073b3c342cc27ad
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-09-21 14:31:33 +0530

    Initial Commit

--- lib/storages in flutter/hive using hive adapter/screens/login_page.dart
+++ lib/storages in flutter/hive using hive adapter/screens/login_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:project_june1/storages%20in%20flutter/hive%20using%20hive%20adapter/screens/RegistrationPAge.dart';
 
