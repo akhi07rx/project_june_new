86572fa351f5e8dfb6d0b306987dd96f86996e68
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-09-22 13:46:08 +0530

    Initial Commit

--- lib/storages in flutter/hive using hive adapter/screens/RegistrationPage.dart
+++ lib/storages in flutter/hive using hive adapter/screens/RegistrationPage.dart
@@ -3,6 +3,45 @@ import 'package:flutter/material.dart';
 class RegisterHive extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final name = TextEditingController();
+    final email = TextEditingController();
+    final pass = TextEditingController();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Registration Page'),
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              controller: name,
+              decoration: const InputDecoration(hintText: 'Name'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              controller: email,
+              decoration: const InputDecoration(hintText: 'Email'),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: TextField(
+              controller: pass,
+              decoration: const InputDecoration(hintText: 'Password'),
+            ),
+          ),
+          ElevatedButton(onPressed: () {}, child: const Text('Login')),
+          TextButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => RegisterHive()));
+              },
+              child: const Text('Not a user? Register Here!')),
+        ],
+      ),
+    );
   }
 }
