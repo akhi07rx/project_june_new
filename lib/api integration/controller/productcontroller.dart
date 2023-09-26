bb254133c368f5b47f4a55eff093f0984811d7a7
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-09-26 15:00:37 +0530

    Initial Commit

--- lib/api integration/controller/productcontroller.dart
+++ lib/api integration/controller/productcontroller.dart
@@ -1,7 +1,28 @@
 import 'package:get/get.dart';
+import 'package:project_june1/api%20integration/service/api_services.dart';
 
 class ProductController extends GetxController {
   //obs - observable
   var isLoading = true.obs;
   var productList = [].obs;
+
+  @override
+  void onInit() {
+    getProducts();
+    super.onInit();
+  }
+
+  void getProducts() async {
+    try {
+      isLoading(true);
+      var products = await HttpServices.fetchProducts();
+      if (products != null) {
+        productList.value = products;
+      }
+    } catch (e) {
+      print(e);
+    } finally {
+      isLoading(false);
+    }
+  }
 }
