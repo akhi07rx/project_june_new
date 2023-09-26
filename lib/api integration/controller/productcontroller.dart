ea0ba69987288443f792cc73e57b2ef163e5fd01
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-09-26 14:53:54 +0530

    Initial Commit

---
+++ lib/api integration/controller/productcontroller.dart
@@ -0,0 +1,8 @@
+import 'package:get/get.dart';
+
+class ProductController extends GetxController{
+  //obs - observable
+  var isLoading = true.obs;
+  var productList = [].obs;
+
+}
--- lib/api integration/service/api_services.dart
+++ lib/api integration/service/api_services.dart
@@ -1,9 +1,17 @@
-import 'package:project_june1/api%20integration/model/productModel.dart';
 import 'package:http/http.dart' as http;
+import 'package:project_june1/api%20integration/model/productModel.dart';
 
 class HttpServices {
   static Future<List<ProductModel>> fetchProducts() async {
-    var response = await http.get(Uri())
-
+    var response =
+        await http.get(Uri.parse("https://fakestoreapi.com/products"));
+    if (response.statusCode == 200) {
+      var data = response.body;
+      return productModelFromJson(data);
+    }
+    else
+      {
+        throw Exception();
+      }
   }
 }
