a6362aaf0010550320919ade7866b98f084f633d
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-09-26 14:37:44 +0530

    Initial Commit

--- lib/api integration/service/api_services.dart
+++ lib/api integration/service/api_services.dart
@@ -1,8 +1,9 @@
 import 'package:project_june1/api%20integration/model/productModel.dart';
+import 'package:http/http.dart' as http;
 
-class HttpServices{
-  static Future<List<ProductModel>> fetchProducts() async{
-
+class HttpServices {
+  static Future<List<ProductModel>> fetchProducts() async {
+    var response = await http.get(Uri())
 
   }
 }
