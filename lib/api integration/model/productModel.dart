8d19154212c36bbc81f9e422d71efb2d90e65053
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-09-26 14:19:01 +0530

    Initial Commit

---
+++ lib/api integration/model/productModel.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+
+List<ProductModel> productModelFromJson(String str) => List<ProductModel>.from(
+    json.decode(str).map((x) => ProductModel.fromJson(x)));
+
+String productModelToJson(List<ProductModel> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class ProductModel {
+  int? id;
+  String? title;
+  double? price;
+  String? description;
+  Category? category;
+  String? image;
+  Rating? rating;
+
+  ProductModel({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.category,
+    this.image,
+    this.rating,
+  });
+
+  factory ProductModel.fromJson(Map<String, dynamic> json) => ProductModel(
+        id: json["id"],
+        title: json["title"],
+        price: json["price"]?.toDouble(),
+        description: json["description"],
+        category: categoryValues.map[json["category"]]!,
+        image: json["image"],
+        rating: json["rating"] == null ? null : Rating.fromJson(json["rating"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "price": price,
+        "description": description,
+        "category": categoryValues.reverse[category],
+        "image": image,
+        "rating": rating?.toJson(),
+      };
+}
+
+enum Category { ELECTRONICS, JEWELERY, MEN_S_CLOTHING, WOMEN_S_CLOTHING }
+
+final categoryValues = EnumValues({
+  "electronics": Category.ELECTRONICS,
+  "jewelery": Category.JEWELERY,
+  "men's clothing": Category.MEN_S_CLOTHING,
+  "women's clothing": Category.WOMEN_S_CLOTHING
+});
+
+class Rating {
+  double? rate;
+  int? count;
+
+  Rating({
+    this.rate,
+    this.count,
+  });
+
+  factory Rating.fromJson(Map<String, dynamic> json) => Rating(
+        rate: json["rate"]?.toDouble(),
+        count: json["count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "rate": rate,
+        "count": count,
+      };
+}
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
+  }
+}
