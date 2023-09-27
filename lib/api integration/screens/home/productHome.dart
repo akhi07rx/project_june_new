81d7d244cdb2787cacf1ed7af838f0ffb932d51f
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-09-27 14:17:56 +0530

    Initial Commit

--- lib/api integration/screens/home/productHome.dart
+++ lib/api integration/screens/home/productHome.dart
@@ -10,21 +10,29 @@ void main() {
 
 class ProductHome extends StatelessWidget {
   final ProductController productController = Get.put(ProductController());
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Products"),),
-      body: SizedBox(child: Obx((){
-        if(productController.isLoading.value){
-          return const Center(child: CircularProgressIndicator(),);
-    }
-        else{
-          return GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), itemBuilder: (context,index){
-            return ProductTile(productController.productList[index]);
-    })
-    }
-    }),
+      appBar: AppBar(
+        title: const Text("Products"),
+      ),
+      body: SizedBox(
+        child: Obx(() {   // Obx is to make the widget observable
+          if (productController.isLoading.value) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          } else {
+            return GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return ProductTile(
+                      productController.productList[index]);
+                });
+          }
+        }),
+      ),
     );
   }
 }
