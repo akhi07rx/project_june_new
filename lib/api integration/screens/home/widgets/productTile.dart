6429daeac4a36f06794c267a40c8364b17ea5471
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-09-27 14:13:29 +0530

    Initial Commit

--- lib/api integration/screens/home/widgets/productTile.dart
+++ lib/api integration/screens/home/widgets/productTile.dart
@@ -1,12 +1,40 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:project_june1/api%20integration/model/productModel.dart';
 
 class ProductTile extends StatelessWidget {
   final ProductModel product;
-ProductTile(this.product);
+
+  ProductTile(this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Card(child: Column(children: [Container(height: 180,width: double.infinity,decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5))),)],),);
+    return Card(
+      child: Column(
+        children: [
+          Container(
+            height: 180,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(5))),
+            child: CachedNetworkImage(
+              imageUrl: product.image!,
+              fit: BoxFit.cover,
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          Text(
+            product.title!,
+            style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+          ),
+          Text(
+            '${product.price!}\$',
+            style: const TextStyle(fontSize: 15),
+          )
+        ],
+      ),
+    );
   }
 }
