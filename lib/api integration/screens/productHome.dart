1c4edacf3ab1a28d8b01f825f1c0d3f733789df7
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-09-27 13:43:17 +0530

    Initial Commit

--- lib/api integration/screens/productHome.dart
+++ lib/api integration/screens/productHome.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: ProductHome(),
+  ));
+}
+
 class ProductHome extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold();
