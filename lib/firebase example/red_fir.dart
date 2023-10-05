706c1fb048ab4e1345823b5177606127ff173df6
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-05 13:56:13 +0530

    Initial Commit

---
+++ lib/firebase example/red_fir.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Register_fire extends StatefulWidget {
+  const Register_fire({super.key});
+
+  @override
+  State<Register_fire> createState() => _Register_fireState();
+}
+
+class _Register_fireState extends State<Register_fire> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Registration"),),
+      body: Column,
+    );
+  }
+}
