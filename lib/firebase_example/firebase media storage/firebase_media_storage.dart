5992ab6b15d7bef709b24c9cbd932e1f2bf39bba
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-10 14:15:36 +0530

    Initial Commit
    Signed-off-by: akhi07rx <89210430+akhi07rx@users.noreply.github.com>

--- lib/firebase_example/firebase media storage/firebase_media_storage.dart
+++ lib/firebase_example/firebase media storage/firebase_media_storage.dart
@@ -11,7 +11,13 @@ class _FirebaseMediaStorage State extends State<FirebaseMediaStorage > {
 @override
 Widget build(BuildContext context) {
 return Scaffold(
-
+appBar: AppBar(title: Text("Storage Media"),),
+  body: Padding(
+    padding: EdgeInsets.all(15),
+    child: Column(
+      children: [],
+    ),
+  ),
 );
 }
 }
