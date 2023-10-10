fecf34b9316b220ece5317bc95af93e7f0261b0d
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-10 14:19:21 +0530

    Initial Commit
    Signed-off-by: akhi07rx <89210430+akhi07rx@users.noreply.github.com>

--- lib/firebase_example/firebase media storage/firebase_media_storage.dart
+++ lib/firebase_example/firebase media storage/firebase_media_storage.dart
@@ -3,21 +3,36 @@ import 'package:flutter/material.dart';
 class FirebaseMediaStorage extends StatefulWidget {
 
 
-@override
-State<FirebaseMediaStorage > createState() => _FirebaseMediaStorage State();
+  @override
+  State<FirebaseMediaStorage> createState() => _FirebaseMediaStorage
+
+  State();
 }
 
-class _FirebaseMediaStorage State extends State<FirebaseMediaStorage > {
+class _FirebaseMediaStorage
+State extends State<FirebaseMediaStorage> {
+
 @override
 Widget build(BuildContext context) {
-return Scaffold(
-appBar: AppBar(title: Text("Storage Media"),),
-  body: Padding(
-    padding: EdgeInsets.all(15),
-    child: Column(
-      children: [],
+  return Scaffold(
+    appBar: AppBar(title: Text("Storage Media"),),
+    body: Padding(
+      padding: EdgeInsets.all(15),
+      child: Column(
+        children: [Row(
+          children: [
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: (Icons.camera_alt_outlined),
+                label: Text("Camera")),
+            ElevatedButton.icon(
+                onPressed: () {},
+                icon: (Icons.photo),
+                label: Text("Gallery")),
+          ],
+        )
+        ],
+      ),
     ),
-  ),
-);
-}
-}
+  );
+}}
