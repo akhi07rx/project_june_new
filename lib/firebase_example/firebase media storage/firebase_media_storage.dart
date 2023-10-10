84512e7d9b607a1e121bd13c15fd037b3625f2cc
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-10 14:12:14 +0530

    Initial Commit
    Signed-off-by: akhi07rx <89210430+akhi07rx@users.noreply.github.com>

---
+++ lib/firebase_example/firebase media storage/firebase_media_storage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FirebaseMediaStorage extends StatefulWidget {
+
+
+@override
+State<FirebaseMediaStorage > createState() => _FirebaseMediaStorage State();
+}
+
+class _FirebaseMediaStorage State extends State<FirebaseMediaStorage > {
+@override
+Widget build(BuildContext context) {
+return Scaffold();
+}
+}
