e30247b89c04c0867e88099e856342fd1c7a8ea4
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-10 15:12:52 +0530

    Initial Commit
    Signed-off-by: akhi07rx <89210430+akhi07rx@users.noreply.github.com>

--- lib/firebase_example/firebase media storage/firebase_media_storage.dart
+++ lib/firebase_example/firebase media storage/firebase_media_storage.dart
@@ -1,71 +1,89 @@
 import 'dart:io';
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:path/path.dart' as path;
 
-class FirebaseMediaStorage extends StatefulWidget {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+        apiKey: "AIzaSyA4EXa4My5mFXw2SeGac3k28Np8fW9rTgc",
+        projectId: "project-june-398208",
+        appId: '1:449474307463:android:c7edd9a6607ad1240edf22',
+        messagingSenderId: '',
+        storageBucket: "project-june-398208.appspot.com"),
+  );
+  runApp(MaterialApp(
+    home: FireMediaStorage(),
+  ));
+}
 
+class FireMediaStorage extends StatefulWidget {
+  const FireMediaStorage({super.key});
 
   @override
-  State<FirebaseMediaStorage> createState() => _FirebaseMediaStorage
-
-  State();
+  State<FireMediaStorage> createState() => _FireMediaStorageState();
 }
 
-class _FirebaseMediaStorage
-State extends State<FirebaseMediaStorage> {
+class _FireMediaStorageState extends State<FireMediaStorage> {
+  FirebaseStorage storage = FirebaseStorage.instance;
 
-FirebaseStorage storage = FirebaseStorage.instance;
-
-@override
-Widget build(BuildContext context) {
-  return Scaffold(
-    appBar: AppBar(title: Text("Storage Media"),),
-    body: Padding(
-      padding: EdgeInsets.all(15),
-      child: Column(
-        children: [Row(
-          children: [
-            ElevatedButton.icon(
-                onPressed: () => upload('camera'),
-                icon: (Icons.camera_alt_outlined),
-                label: Text("Camera")),
-            ElevatedButton.icon(
-                onPressed: () => upload('camera'),
-                icon: (Icons.photo),
-                label: Text("Gallery")),
-          ],
-        )
-        ],
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FireBase Storage'),
       ),
-    ),
-  );
-}
-
-Future<void> upload(String imageSource) async {
-  final picker = ImagePicker();
-  XFile? pickedImage;
-
-  try {
-    pickedImage = await picker.pickImage(source: imageSource == 'camera' ?
-    ImageSource.camera : ImageSource.gallery, maxWidth: 1920
+      body: Padding(
+        padding: EdgeInsets.all(15),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                ElevatedButton.icon(
+                    onPressed: () => upload('camera'),
+                    icon: Icon(Icons.camera_alt_rounded),
+                    label: Text('Camera')),
+                ElevatedButton.icon(
+                    onPressed: () => upload('gallery'),
+                    icon: Icon(Icons.photo_album),
+                    label: Text('Gallery')),
+              ],
+            )
+          ],
+        ),
+      ),
     );
-    final String filename = path.basename(pickedImage!.path);
-    File imagefile = File(pickedImage.path)
+  }
+
+  Future<void> upload(String imageSource) async {
+    final picker = ImagePicker();
+    XFile? pickedImage;
 
     try {
-      await storage.ref(fileName).putFile(
-          imagefile, SettableMetadata(customMetadata: {
-        'UploadedBy': "Its ME XXXX",
-        'Description': "Some Description"
-      })
-      );
-      setState(() {});
-    }
-    on FirebaseException catch (error) {
-      print(error)
+      pickedImage = await picker.pickImage(
+          source: imageSource == 'camera'
+              ? ImageSource.camera
+              : ImageSource.gallery,
+          maxWidth: 1920);
+      final String filename = path.basename(pickedImage!.path);
+      File imagefile = File(pickedImage.path);
+      try {
+        await storage.ref(filename).putFile(
+            imagefile,
+            SettableMetadata(customMetadata: {
+              'uploaded by': 'its me xxxx',
+              'description': 'some description'
+            }));
+        setState(() {});
+      } on FirebaseException catch (error) {
+        print(error);
+      }
+    } catch (error) {
+      print(error);
     }
   }
-}}
+}
