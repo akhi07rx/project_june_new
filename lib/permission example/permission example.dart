3fdfb68b3c6ae28fa4f53f717cae966e6241fe1d
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-12 14:17:32 +0530

    Initial Commit
    Signed-off-by: akhi07rx <89210430+akhi07rx@users.noreply.github.com>

---
+++ lib/permission example/permission example.dart
@@ -0,0 +1,13 @@
+class PermissionHandler extends StatefulWidget {
+  const PermissionHandler ({super.key});
+
+  @override
+  State<PermissionHandler > createState() => _PermissionHandler State();
+}
+
+class _PermissionHandler State extends State<PermissionHandler > {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
