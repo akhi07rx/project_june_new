4b5f595cdc9bb65106331b7295e0308facc40a15
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-14 10:18:53 +0530

    Initial Commit
    Signed-off-by: akhi07rx <89210430+akhi07rx@users.noreply.github.com>

--- lib/statemanagement using provider/provider/movieProvider.dart
+++ lib/statemanagement using provider/provider/movieProvider.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
+import 'package:project_june1/statemanagement%20using%20provider/model/models.dart';
 
+final List<Movies> movieList = List.generate(100, (index) => Movies(title: 'Movie $index'));
 class MovieProvider extends ChangeNotifier{
 
 }
