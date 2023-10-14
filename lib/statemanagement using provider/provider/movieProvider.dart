db4a99743d3a2e198f165589152a4be6e97ca132
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-14 10:21:15 +0530

    Initial Commit
    Signed-off-by: akhi07rx <89210430+akhi07rx@users.noreply.github.com>

--- lib/statemanagement using provider/provider/movieProvider.dart
+++ lib/statemanagement using provider/provider/movieProvider.dart
@@ -1,7 +1,11 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:project_june1/statemanagement%20using%20provider/model/models.dart';
 
-final List<Movies> movieList = List.generate(100, (index) => Movies(title: 'Movie $index'));
-class MovieProvider extends ChangeNotifier{
+final List<Movies> movieList =
+List.generate(100, (index) =>
+    Movies(
+        title: 'Movie $index', time: '${Random().nextInt(100) = 60} minutes'));
 
-}
+class MovieProvider extends ChangeNotifier {}
