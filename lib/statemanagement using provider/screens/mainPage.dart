3335e2411823c7158aa93dd260d712aca7569ea0
akhi07rx <89210430+akhi07rx@users.noreply.github.com>
2023-10-14 10:52:58 +0530

    Initial Commit
    Signed-off-by: akhi07rx <89210430+akhi07rx@users.noreply.github.com>

---
+++ lib/statemanagement using provider/screens/mainPage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieMain extends StatelessWidget {
+  const MovieMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: AppBar(
+        title: Text("Movies"),
+      ),
+      body: Column(),
+    );
+  }
+}
